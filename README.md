e584bda453dded479b3642c58880e2e561dc691f
Ryan <23496457+ryanclouser@users.noreply.github.com>
2023-04-26 15:02:34 -0400

    stt / tts

--- README.md
+++ README.md
@@ -18,36 +18,63 @@ Requirements
 
 1. RTL-SDR or equivalent
 1. GQRX
-1. Python 3.x
+1. Python 3.4+
+
+STT / TTS
+---
+
+1. torch
+1. torchaudio
+1. sounddevice
 
 Usage
 ---
 
+1. `pip3 install -r requirements.txt`
 1. Start GQRX and enable remote control
 1. Run the script
    - `python3 ghostbox.py --fm --speed 150 --squelch -30 --bounce`
 
 ```
-usage: ghostbox.py [-h] [--version] [--ip IP] [--port PORT] [--fm] [--am]
-                   [--speed SPEED] [--squelch SQUELCH] [--random] [--forward]
-                   [--backward] [--bounce]
+usage: ghostbox.py [-h] [--version] [--ip IP] [--port PORT] [--fm] [--am] [--speed SPEED] [--squelch SQUELCH] [--random] [--forward] [--backward] [--bounce] [--record] [--record-interval RECORD_INTERVAL] [--wordlist] [--tts]
 
 Python Ghostbox
 
 options:
-  -h, --help         show this help message and exit
-  --version          Version
-  --ip IP            GQRX IP
-  --port PORT        GQRX port
-  --fm               Enable FM radio scanning
-  --am               Enable AM radio scanning
-  --speed SPEED      Scanning speed in milliseconds
-  --squelch SQUELCH  Squelch
-  --random           Random scanning
-  --forward          Forward scanning
-  --backward         Backward scanning
-  --bounce           Bounce scanning
+  -h, --help            show this help message and exit
+  --version             Version
+  --ip IP               GQRX IP
+  --port PORT           GQRX port
+  --fm                  Enable FM radio scanning
+  --am                  Enable AM radio scanning
+  --speed SPEED         Scanning speed in milliseconds
+  --squelch SQUELCH     Squelch
+  --random              Random scanning
+  --forward             Forward scanning
+  --backward            Backward scanning
+  --bounce              Bounce scanning
+  --record              Record and process audio
+  --record-interval RECORD_INTERVAL
+                        Time for each audio recording
+  --wordlist            Use a wordlist after audio is processed
+  --tts                 Use text to speech
 ```
+
+Recording
+---
+
+The Ghostbox is capable of speech recognition by processing the output WAV file from GQRX. The default behavior is to record for 5s, stop recording, process the WAV, delete the file, then start recording again. GQRX can be muted and recordings will function normally. Any pull requests to stream the WAV directly into the ASR would be more than welcome :)
+
+```
+python3 ghostbox.py --fm --speed 150 --squelch -30 --bounce --record --wordlist --tts
+```
+
+Wordlist
+---
+
+Top 5000 common English words.
+
+- https://github.com/filiph/english_words/
 
 License
 ---
--- ghostbox.py
+++ ghostbox.py
@@ -3,8 +3,12 @@ import telnetlib
 import time
 import random
 import datetime
+import glob
+import os
+import csv
+from pathlib import Path
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 AM_START = 540000
 AM_END = 1700000
@@ -13,6 +17,99 @@ AM_STEP = 10000
 FM_START = 88100000
 FM_END = 108000000
 FM_STEP = 200000
+
+WAV_PATH = os.path.join(Path.home(), 'gqrx_*.wav')
+
+
+try:
+	import torch
+	import torchaudio
+	import torch.multiprocessing as mp
+	import sounddevice as sd
+
+	bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	model = bundle.get_model().to(device)
+	resampler = torchaudio.transforms.Resample(48_000, 16_000)
+
+	tts_model = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_tts', language='en', speaker='v3_en', verbose=False)[0]
+	tts_model.to(device)
+	TORCH = True
+except Exception as e:
+	TORCH = False
+	print(e)
+
+
+# https://pytorch.org/audio/main/tutorials/asr_inference_with_ctc_decoder_tutorial.html
+class GreedyCTCDecoder(torch.nn.Module):
+	def __init__(self, labels, blank=0):
+		super().__init__()
+		self.labels = labels
+		self.blank = blank
+
+	def forward(self, emission: torch.Tensor) -> list[str]:
+		"""Given a sequence emission over labels, get the best path
+		Args:
+		emission (Tensor): Logit tensors. Shape `[num_seq, num_label]`.
+
+		Returns:
+		List[str]: The resulting transcript
+		"""
+		indices = torch.argmax(emission, dim=-1)  # [num_seq,]
+		indices = torch.unique_consecutive(indices, dim=-1)
+		indices = [i for i in indices if i != self.blank]
+		joined = ''.join([self.labels[i] for i in indices])
+		return joined.replace('|', ' ').strip().lower().split()
+
+
+def infer(path):
+	waveform, sample_rate = torchaudio.load(path)
+	waveform = resampler(waveform).squeeze()
+
+	with torch.inference_mode():
+		features, _ = model.extract_features(waveform)
+	with torch.inference_mode():
+		emission, _ = model(waveform)
+
+	decoder = GreedyCTCDecoder(labels=bundle.get_labels())
+	return decoder(emission[0])
+
+
+def tts(text, blocking=True):
+	sample_rate = 48000
+	speaker = 'en_0'
+
+	audio = tts_model.apply_tts(text=text, speaker=speaker, sample_rate=sample_rate)
+	sd.play(audio, sample_rate, blocking=blocking)
+
+
+def process(q, use_wordlist, use_tts):
+	words = []
+	if use_wordlist:
+		try:
+			with open('word-freq-top5000.csv', newline='') as csvfile:
+				reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+				for row in reader:
+					words.append(row[1])
+		except Exception as e:
+			print(e)
+
+	while True:
+		f = q.get()
+		result = infer(f)
+		data = []
+
+		for word in result:
+			if not words or word in words:
+				data.append(word)
+
+		if data:
+			print('{} -> {}'.format(datetime.datetime.now(), ' '.join(data)))
+			if use_tts:
+				tts('.'.join(data) + '.')
+
+		os.remove(f)
+
 
 class Ghostbox:
 	def __init__(self, ip, port):
@@ -31,13 +128,18 @@ class Ghostbox:
 	def set_mode(self, mode):
 		return self._write('M {}\n'.format(mode))
 
+	def set_record(self, mode):
+		return self._write('U RECORD {}\n'.format(mode))
+
 	def get_strength(self):
-		return self._write('l\n')
+		return float(self._write('l\n'))
+
 
 def gb_freq(gb, freq, mode=None):
 	if mode:
 		gb.set_mode(mode)
 	gb.set_freq(freq)
+
 
 def main():
 	parser = argparse.ArgumentParser(description='Python Ghostbox')
@@ -52,6 +154,10 @@ def main():
 	parser.add_argument('--forward', action='store_true', help='Forward scanning')
 	parser.add_argument('--backward', action='store_true', help='Backward scanning')
 	parser.add_argument('--bounce', action='store_true', default=True, help='Bounce scanning')
+	parser.add_argument('--record', action='store_true', help='Record and process audio')
+	parser.add_argument('--record-interval', type=float, default=5.0, help='Time for each audio recording')
+	parser.add_argument('--wordlist', action='store_true', help='Use a wordlist after audio is processed')
+	parser.add_argument('--tts', action='store_true', help='Use text to speech')
 	args = parser.parse_args()
 
 	if args.version:
@@ -62,8 +168,10 @@ def main():
 		print('Error: AM/FM frequencies not enabled')
 		return
 
-	if args.speed <= 0:
+	if args.speed < 0:
 		print('Error: Invalid scan speed')
+	elif args.speed > 0:
+		args.speed /= 1000
 
 	print("   _____ _               _   _               ")
 	print("  / ____| |             | | | |              ")
@@ -89,10 +197,23 @@ def main():
 	else:
 		print('- Bounce')
 
-	print('- {}ms interval'.format(args.speed))
-	print('- {}dB squelch\n'.format(args.squelch))
+	if args.speed > 0:
+		print('- {}s interval'.format(args.speed))
+	else:
+		print('- Random interval')
 
-	args.speed /= 1000
+	print('- {}dB squelch'.format(args.squelch))
+
+	if args.record:
+		if TORCH:
+			print('- Recording')
+			print('- {}s record interval'.format(args.record_interval))
+		else:
+			args.record = False
+			print('Torch is not installed, cannot process recordings')
+
+	print()
+
 	frequencies = []
 
 	if args.am:
@@ -113,14 +234,30 @@ def main():
 		else:
 			gb.set_mode('WFM')
 
-	start = datetime.datetime.now()
-	print('Started @ {}'.format(start))
+	if args.record:
+		ctx = mp.get_context('spawn')
+		q = ctx.Queue()
+		p = ctx.Process(target=process, args=(q, args.wordlist, args.tts))
+		p.start()
 
-	freq = 0
-	index = 0
-	direction = 1
+	start = datetime.datetime.now()
+	print('Started @ {}\n'.format(start))
 
 	try:
+		freq = 0
+		index = 0
+		direction = 1
+
+		if args.record:
+			gb.set_record(0)
+			for f in glob.glob(WAV_PATH):
+				print('Removing {}'.format(f))
+				os.remove(f)
+			print()
+
+			record_start = time.time()
+			gb.set_record(1)
+
 		while True:
 			if args.random:
 				freq = random.choice(frequencies)
@@ -140,6 +277,14 @@ def main():
 				freq = frequencies[index]
 				index += direction
 
+			if args.record and time.time() - record_start >= args.record_interval:
+				gb.set_record(0)
+				files = glob.glob(WAV_PATH)
+				files.sort(key=os.path.getmtime)
+				q.put(files[-1])
+				record_start = time.time()
+				gb.set_record(1)
+
 			if freq < FM_START:
 				gb_freq(gb, freq, 'AM' if mode else None)
 			else:
@@ -150,12 +295,20 @@ def main():
 				# spooooky
 				random.seed(gb.get_strength())
 				time.sleep(args.speed / 2)
-			else:
+			elif args.speed > 0:
 				time.sleep(args.speed)
+			else:
+				time.sleep(0.5 + gb.get_strength() / 100)
 	except KeyboardInterrupt:
-		end = datetime.datetime.now()
-		print('Ended @ {}\n'.format(end))
-		print('Runtime {}'.format(end - start))
+		pass
+
+	end = datetime.datetime.now()
+	print('Ended @ {}\n'.format(end))
+	print('Runtime {}'.format(end - start))
+
+	if args.record:
+		p.join()
+
 
 if __name__ == '__main__':
 	main()
